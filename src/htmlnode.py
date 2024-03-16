a44b135d8c62a029fa97ee0053858b6229fc33aa
Leslie Poolman <leslie.john.poolman01@gmail.com>
2024-03-15 22:06:01 -0500

    refactor due to addition of leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,38 +1,25 @@
 class HTMLNode():
-    def __init__(self, tag=None, value=None):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
-        self.__children = []
-        self.__props = {}
+        self.children = children
+        self.props = props
 
     def to_html(self):
         raise NotImplementedError()
     
     def props_to_html(self):
-        if self.__props is None:
+        if self.props is None:
             return
         result = ""
-        for key, value in self.__props.items():
+        for key, value in self.props.items():
             result += f" {key}=\"{value}\""
         return result
     
     def __repr__(self):
-        result = f"tag={self.tag} | props={self.__props} | value={self.value} | children="
-        if len(self.__children) > 0:
-            for child in self.__children:
+        result = f"tag={self.tag} | props={self.props} | value={self.value} | children="
+        if self.children and len(self.children) > 0:
+            for child in self.children:
                 result += f"\n->-> {child}"
         return result
     
-    def add_child(self, child):
-        if child is None:
-            return
-        self.__children.append(child)
-    
-    def add_props(self, properties):
-        if properties is None:
-            return
-        for key, value in properties.items():
-            if key in self.__props.keys():
-                self.__props[key] += f" {value}"
-            else:
-                self.__props[key] = value
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,18 +4,14 @@ from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr_with_child(self):
-        node = HTMLNode("table")
-        node2= HTMLNode("thead", "Header")
-        node.add_props({ "class": "table" })
-        node2.add_props({ "scope": "col" })
-        node.add_child(node2)
+        node2= HTMLNode("thead", "Header", None, { "scope": "col" })
+        node = HTMLNode("table", None, [node2], { "class": "table" })
         expected_string = "tag=table | props={'class': 'table'} | value=None | children="
         expected_string += f"\n->-> tag=thead | props={{'scope': 'col'}} | value=Header | children="
         self.assertEqual(str(node), expected_string)
 
     def test_props_to_html(self):
-        node = HTMLNode("table")
-        node.add_props({ "class": "table", "id": "test_table" })
+        node = HTMLNode("table", None, None, { "class": "table", "id": "test_table" })
         self.assertEqual(node.props_to_html(), " class=\"table\" id=\"test_table\"")
 
     def test_props_to_html_guard(self):
