b987ee48ad2f06f5b8d0f981906298ce8f4bbcc0
Leslie Poolman <leslie.john.poolman01@gmail.com>
2024-03-25 15:49:45 -0500

    update main imports

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
 from textnode import TextNode
+from leafnode import LeafNode
+from parentnode import ParentNode
 from htmlnode import HTMLNode
 
 def main():
-    textnode = TextNode("This is a text node", "bold", "https://example.com")
-    htmlnode = HTMLNode("table")
-    child = HTMLNode("thead", "header")
-    child.add_props({ "scope": "col" })
-    htmlnode.add_child(child)
-    htmlnode.add_props({ "class": "table", "id": "tester"})
-    print(textnode)
-    print(htmlnode)
-    print(htmlnode.props_to_html())
+    node = ParentNode(
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+        "p",
+    )
+
+    print(node.to_html())
 
 main()
