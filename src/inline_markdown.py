5242c2a80e7f69ce3216e63d62f09214b5a1a0eb
Leslie Poolman <leslie.john.poolman01@gmail.com>
2024-04-14 15:50:47 -0500

    bugfixes and refactors to cover edge cases

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -7,6 +7,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.texttype != "text":
             result.append(node)
+            continue
         split_text = node.text.split(delimiter)
         if len(split_text) % 2 == 0:
             raise Exception("Invalid Markdown, missing closing delimiter")
@@ -31,13 +32,19 @@ def extract_markdown_links(text):
 def split_nodes_image(old_nodes):
     result = []
     for node in old_nodes:
+        if node.texttype != "text":
+            result.append(node)
+            continue
         tuples = extract_markdown_images(node.text)
         if len(tuples) == 0:
             result.append(node)
+            continue
         else:
             node_text = node.text
             for tup in tuples:
                 txt_lst = node_text.split(f"![{tup[0]}]({tup[1]})", 1)
+                if len(txt_lst) != 2:
+                    raise Exception("Invalid Markdown, Image not properly closed")
                 if txt_lst[0] != "":
                     result.append(TextNode(txt_lst[0], "text"))
                 result.append(TextNode(tup[0], "image", tup[1]))
@@ -50,13 +57,19 @@ def split_nodes_image(old_nodes):
 def split_nodes_link(old_nodes):
     result = []
     for node in old_nodes:
+        if node.texttype != "text":
+            result.append(node)
+            continue
         tuples = extract_markdown_links(node.text)
         if len(tuples) == 0:
             result.append(node)
+            continue
         else:
             node_text = node.text
             for tup in tuples:
                 txt_lst = node_text.split(f"[{tup[0]}]({tup[1]})", 1)
+                if len(txt_lst) != 2:
+                    raise Exception("Invalid Markdown, link not properly closed")
                 if txt_lst[0] != "":
                     result.append(TextNode(txt_lst[0], "text"))
                 result.append(TextNode(tup[0], "link", tup[1]))
