141da2e6d825fd324e888d9d2d663a92d630e4a3
Leslie Poolman <leslie.john.poolman01@gmail.com>
2024-04-24 14:01:48 -0500

    Simple implementation, not covering all use cases effectively

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,23 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+block_type_from_md_tag = {
+    "#": block_type_heading,
+    "##": block_type_heading,
+    "###": block_type_heading,
+    "####": block_type_heading,
+    "#####": block_type_heading,
+    "######": block_type_heading,
+    "```": block_type_code,
+    ">": block_type_quote,
+    "*": block_type_unordered_list,
+    "-": block_type_unordered_list,
+}
+
 def markdown_to_blocks(markdown):
     blocks = []
     for block in markdown.split("\n\n"):
@@ -6,3 +26,11 @@ def markdown_to_blocks(markdown):
         clean_block = block.strip()
         blocks.append(clean_block)
     return blocks
+
+def block_to_block_type(block):
+    md_tag = block.split()[0]
+    if md_tag in block_type_from_md_tag.keys():
+        return block_type_from_md_tag[md_tag]
+    if md_tag[-1] == "." and any(c.isdigit() for c in md_tag[:-1]):
+        return block_type_ordered_list
+    return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -2,6 +2,13 @@ import unittest
 
 from markdown_blocks import (
     markdown_to_blocks,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_quote,
+    block_type_unordered_list,
+    block_type_ordered_list,
 )
 
 
@@ -42,6 +49,58 @@ This is **bolded** paragraph
             "* This is a list\n* with items",
         ]
         self.assertEqual(exp_res, example)
+
+    def test_block_to_block_type_heading(self):
+        example = block_to_block_type("# This is a heading")
+        self.assertEqual(block_type_heading, example)
+
+    def test_block_to_block_type_heading2(self):
+        example = block_to_block_type("## This is a heading")
+        self.assertEqual(block_type_heading, example)
+
+    def test_block_to_block_type_heading3(self):
+        example = block_to_block_type("### This is a heading")
+        self.assertEqual(block_type_heading, example)
+
+    def test_block_to_block_type_heading4(self):
+        example = block_to_block_type("#### This is a heading")
+        self.assertEqual(block_type_heading, example)
+
+    def test_block_to_block_type_heading5(self):
+        example = block_to_block_type("##### This is a heading")
+        self.assertEqual(block_type_heading, example)
+
+    def test_block_to_block_type_heading6(self):
+        example = block_to_block_type("###### This is a heading")
+        self.assertEqual(block_type_heading, example)
+
+    def test_block_to_block_type_code(self):
+        example = block_to_block_type("``` This is a code block")
+        self.assertEqual(block_type_code, example)
+
+    def test_block_to_block_type_quote(self):
+        example = block_to_block_type("> This is a quote block")
+        self.assertEqual(block_type_quote, example)
+
+    def test_block_to_block_type_unordered_list(self):
+        example = block_to_block_type("* This is an unordered list item")
+        self.assertEqual(block_type_unordered_list, example)
+
+    def test_block_to_block_type_unordered_list2(self):
+        example = block_to_block_type("- This is an unordered list item")
+        self.assertEqual(block_type_unordered_list, example)
+
+    def test_block_to_block_type_ordered_list(self):
+        example = block_to_block_type("1. This is an ordered list item")
+        self.assertEqual(block_type_ordered_list, example)
+
+    def test_block_to_block_type_ordered_list2(self):
+        example = block_to_block_type("10. This is an ordered list item")
+        self.assertEqual(block_type_ordered_list, example)
+
+    def test_block_to_block_type_paragraph(self):
+        example = block_to_block_type("This is a paragraph.")
+        self.assertEqual(block_type_paragraph, example)
 
 
 if __name__ == "__main__":
